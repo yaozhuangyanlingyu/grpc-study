43eb63a9895a56c177d1cc94a0188963d5d98085
yaoxf <850630887@qq.com>
2020-01-09 00:11:23 +0800

    临时提交

---
+++ greeter_server/main.go
@@ -0,0 +1,38 @@
+package main
+
+import(
+    "context"
+    pb "grpc-helloworld/hellowrold"
+    "net"
+    "google.golang.org/grpc"
+    "google.golang.org/grpc/reflection"
+)
+
+const(
+    port = ":8080"
+)
+
+type server struct{}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+    return &pb.HelloReply{Message : "Hello" + in.Name}, nil
+}
+
+func main() {
+    // 指定执行程序监听的端口
+    lis, err := net.Listen("tcp", port)
+    if err != nil {
+        fmt.Println(err.Error())
+        return
+    }
+
+    // 建立gRPC服务器，并注册服务
+    s := grpc.NewServer()
+    pb.RegisterGreetServer(s, &server)
+    fmt.Println("Server run ...")
+
+    // 启动服务
+    if err := s.Server(lis); err != nil {
+        fmt.Println(err.Error())
+    }
+}
